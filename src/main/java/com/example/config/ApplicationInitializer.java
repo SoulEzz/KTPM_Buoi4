dd586cb11d2a83be71fa2e202b3413e03f710912
SoulEzz <vuaharem1.0@gmail.com>
2022-10-03 11:30:06 +0700

    khong the chay duoc

---
+++ src/main/java/com/example/config/ApplicationInitializer.java
@@ -0,0 +1,25 @@
+package com.example.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// TODO Auto-generated method stub
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(JmsConfig.class);
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+	}
+	
+}
---
+++ src/main/java/com/example/jms/Producer.java
@@ -0,0 +1,29 @@
+package com.example.jms;
+
+import java.util.Map;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Producer {
+	@Autowired
+    private JmsTemplate jmsTemplate;
+
+    public void sendMessage(String queueName, final String message) {
+        Map map = new Gson().fromJson(message, Map.class);
+        final String textMessage = "Chào nhé " + map.get("name");
+        System.out.println("Gửi tin nhắn " + textMessage + " đến queue - " + queueName);
+        jmsTemplate.send(queueName, new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage(message);
+            }
+        });
+    }
+}
